9ac5d555c58fa6610599c2611298e10747b9f14f
Viktor Chayun <Viktor.Chayun@materialise.com>
2025-06-19 18:29:57 +0300

    settings. change

--- basic_project/basic_project/settings.py
+++ basic_project/basic_project/settings.py
@@ -125,11 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "note_app/static/note_app/",
+]
