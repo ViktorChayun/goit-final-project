a7e9fd829865c134c32fbe4d0b4b0d77f40519db
Viktor Chayun <Viktor.Chayun@materialise.com>
2025-06-20 22:55:38 +0300

    fixed bug with static settings

--- basic_project/basic_project/settings.py
+++ basic_project/basic_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # важливо для Render
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # важливо для Render
 # Де шукати додаткові статичні файли
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
