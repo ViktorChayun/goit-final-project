467c958557d6810c49581f4ac4af08e63fec9225
Viktor Chayun <Viktor.Chayun@materialise.com>
2025-06-26 13:30:00 +0300

    v0.7.1

--- basic_project/basic_project/settings.py
+++ basic_project/basic_project/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = env("SECRET_KEY")
 UPLOADCARE_PUBLIC_KEY = env('PUBLIC_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
@@ -139,6 +139,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # важливо для Render
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
     BASE_DIR / "note_app/static/note_app/",
+    BASE_DIR / "docs/_build/html",
 ]
 
 # Default primary key field type
